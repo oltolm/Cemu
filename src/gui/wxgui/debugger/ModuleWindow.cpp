a6616d9008a746104ec81a0a1c7329bffdce7fa7
oltolm <oleg.tolmatcev@gmail.com>
2025-11-26 23:39:45 +0100

    UI: remove unnecessary SetSizeHints calls

--- src/gui/wxgui/debugger/ModuleWindow.cpp
+++ src/gui/wxgui/debugger/ModuleWindow.cpp
@@ -21,8 +21,6 @@ enum ItemColumns
 ModuleWindow::ModuleWindow(DebuggerWindow2& parent, const wxPoint& main_position, const wxSize& main_size)
 	: wxFrame(&parent, wxID_ANY, _("Modules"), wxDefaultPosition, wxSize(420, 250), wxSYSTEM_MENU | wxCAPTION | wxCLIP_CHILDREN | wxRESIZE_BORDER | wxFRAME_FLOAT_ON_PARENT)
 {
-	this->SetSizeHints(wxDefaultSize, wxDefaultSize);
-
 	wxBoxSizer* main_sizer = new wxBoxSizer(wxVERTICAL);
 
 	m_modules = new wxListView(this, wxID_ANY);
